9c00697b5fbbf82fd5d4083df5dc8ec9b837d8ea
quansun <quansun1993@gmail.com>
2018-08-07 00:03:06 -0700

    keep modifying about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -25,23 +25,30 @@ const AboutPage = (props, context) => {
         <div className="about-wrapper">
           <div className="about-content" style={{ color: textPrimary }}>
             <p>
-              My main language is{' '}
-              <span className="highlight">Java</span>, but Im also a big fan of{' '}
+              Main{' '}
+              <span className="highlight">Java</span>, big fan of{' '}
               <span className="highlight">JavaScript</span> and{' '}
               <span className="highlight">Python</span>.
             </p>
             <p>
-              I like everything open-source. And I like to "open source" everything.
-              I never stop learning. When I am free, I will bury myself in cutting-edge technologies.
+              I like everything{' '}
+              <span className="highlight">open-source</span>. And I like to "open source" everything.
+              I never stop learning. When I am free, I will bury myself in cutting-edge technologies, like{' '}
+              <span className="highlight">blockchain</span>.
               Besides, I stay close to the community and try to keep tabs with the pace at which the web is evolving.
             </p>
             <p>
               "The faintest ink is better than the best memory." --- I also like to{' '}
-              <span className="highlight">blog</span> what I learn. Please check out my <a target="_blank" rel="noopener noreferrer" href="https://mabuxi.gitbook.io/mabuxi/">Gitbook</a>.
+              <span className="highlight">blog</span> what I learn. Please check out my{' '}
+              <a target="_blank" rel="noopener noreferrer" href="https://mabuxi.gitbook.io/mabuxi/"><span className="highlight">Gitbook</span></a>.
             </p>
             <p>
-              I have a broad range of hands-on experience including Web Application Development, Cloud Computing, Big Data Processing,
-              Machine Learning, and Blokchain Application Development.
+              I have a broad range of hands-on experience including{' '}
+              <span className="highlight">Web Application Development</span>,{' '}
+              <span className="highlight">Cloud Computing</span>,{' '}
+              <span className="highlight">Big Data Processing</span>,{' '}
+              <span className="highlight">Machine Learning</span>, and{' '}
+              <span className="highlight">Blokchain Application Development</span>.
             </p>
             <p>
               Java, Spring, MongoDB, MySQL, Redis, React, Angular2, and Node.js are my main stacks.
