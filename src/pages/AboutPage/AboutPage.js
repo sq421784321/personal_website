0be26e820afd70776b3da66d77dc5669625203b4
quansun <quansun1993@gmail.com>
2018-08-07 00:20:33 -0700

    about page v3

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -51,22 +51,22 @@ const AboutPage = (props, context) => {
               <span className="highlight">Blokchain Application Development</span>.
             </p>
             <p>
-              <i>Java</i>,
-              <i>Spring</i>,
-              <i>Docker</i>,
-              <i>MongoDB</i>,
-              <i>MySQL</i>,
-              <i>Redis</i>,
-              <i>React</i>,
-              <i>Angular2</i>,
-              <i>Node.js</i>, and
+              <i>Java</i>,{' '}
+              <i>Spring</i>,{' '}
+              <i>Docker</i>,{' '}
+              <i>MongoDB</i>,{' '}
+              <i>MySQL</i>,{' '}
+              <i>Redis</i>,{' '}
+              <i>React</i>,{' '}
+              <i>Angular2</i>,{' '}
+              <i>Node.js</i>, and{' '}
               <i>AWS</i> are my main stacks.
             </p>
             <p>
               Elasticsearch, Logstash, Kibana, Nagios, Hadoop, Spark, Lucene, and Tensorflow are my favorite tools.
             </p>
             <p>
-              I love ping pong, basketball, badminton, bicycling, hiking and video games.
+              Programming is just a small part of my life. I love ping pong, basketball, badminton, bicycling, hiking and video games as well.
             </p>
             <p className="text-emoji" style={{ color: colorPrimary }}>
               （￣▽￣）／
